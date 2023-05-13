97208c83cf54e6dabc9ef5cc350b52b14654722c
dpointtt <64966626+dpointtt@users.noreply.github.com>
2023-05-13 20:52:05 +0300

    Added imageView.js to show full size image when user click on it(Task 2)

---
+++ task2/js/imageView.js
@@ -0,0 +1,20 @@
+const gridItems = document.getElementsByClassName("image");
+
+let overlay = document.getElementById("overlay");
+let imageContainer = document.getElementById("image-container");
+let imageToShow = document.getElementById("image-to-show");
+let closeButton = document.getElementById("overlay-close");
+
+for (let i = 0; i < gridItems.length; i++) {
+    let image = gridItems[i].getElementsByTagName("img")[0];
+    image.addEventListener("click", function() {
+        imageToShow.src = this.src;
+        overlay.style.display = "block";
+        imageContainer.style.display = "block";
+
+        closeButton.addEventListener("click", function() {
+            imageContainer.style.display = "none";
+            overlay.style.display = "none";
+        });
+    });
+}
